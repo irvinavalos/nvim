37a1d5a31e53a9e04dddc97ae92c2163c4738b0f
Irvin <irvin.avalos213@gmail.com>
2026-01-07 01:08:13 -0800

    switched to kanso colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,13 +1,14 @@
 return {
-    -- {
-    --     "webhooked/kanso.nvim",
-    --     lazy = false,
-    --     priority = 100,
-    -- },
     {
-        "vague-theme/vague.nvim",
+        "webhooked/kanso.nvim",
         lazy = false,
-        priority = 1000,
-        config = function() vim.cmd.colorscheme("vague") end,
+        priority = 100,
+        config = function() vim.cmd.colorscheme("kanso-zen") end,
     },
+    -- {
+    --     "vague-theme/vague.nvim",
+    --     lazy = false,
+    --     priority = 1000,
+    --     config = function() vim.cmd.colorscheme("vague") end,
+    -- },
 }
