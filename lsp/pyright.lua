931e707487d80e7bd53a75dcd6b9e99680241c94
Irvin <irvin.avalos213@gmail.com>
2026-01-21 01:24:05 -0800

    update

--- lsp/pyright.lua
+++ lsp/pyright.lua
@@ -1,47 +1,58 @@
 local function set_python_path(path)
-	local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf(), name = "pyright" })
-	for _, client in ipairs(clients) do
-		if client.settings then
-			client.settings.python = vim.tbl_deep_extend("force", client.settings.python, { pythonPath = path })
-		else
-			client.config.settings =
-				vim.tbl_deep_extend("force", client.config.settings, { python = { pythonPath = path } })
-		end
-		client.notify("workspace/didChangeConfiguration", { settings = nil })
-	end
+    local clients = vim.lsp.get_clients({ bufnr = vim.api.nvim_get_current_buf(), name = "pyright" })
+    for _, client in ipairs(clients) do
+        if client.settings then
+            client.settings.python =
+                vim.tbl_deep_extend("force", client.settings.python --[[@as table]], { pythonPath = path })
+        else
+            client.config.settings =
+                vim.tbl_deep_extend("force", client.config.settings, { python = { pythonPath = path } })
+        end
+        client.notify("workspace/didChangeConfiguration", { settings = nil })
+    end
 end
 
 ---@type vim.lsp.Config
 return {
-	cmd = { "pyright-langserver", "--stdio" },
-	filetypes = { "python" },
-	root_markers = {
-		"pyproject.toml",
-		"setup.py",
-		"setup.cfg",
-		"requirements.txt",
-		"Pipfile",
-		"pyrightconfig.json",
-		".git",
-	},
-	settings = {
-		pyright = {
-			disableOrganizeImports = true,
-		},
-		python = {
-			analysis = {
-				ignore = { "*" },
-				autoSearchPaths = true,
-				useLibraryCodeForTypes = true,
-				diagnosticMode = "openFilesOnly",
-			},
-		},
-	},
-	on_attach = function(_, bufnr)
-		vim.api.nvim_buf_create_user_command(bufnr, "LspPyrightSetPythonPath", set_python_path, {
-			desc = "Reconfigure pyright with the provided python path",
-			nargs = 1,
-			complete = "file",
-		})
-	end,
+    cmd = { "pyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    root_markers = {
+        "pyproject.toml",
+        "setup.py",
+        "setup.cfg",
+        "requirements.txt",
+        "Pipfile",
+        "pyrightconfig.json",
+        ".git",
+    },
+    settings = {
+        pyright = {
+            disableOrganizeImports = true,
+        },
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                diagnosticMode = "openFilesOnly",
+            },
+        },
+    },
+    on_attach = function(client, bufnr)
+        vim.api.nvim_buf_create_user_command(bufnr, "LspPyrightOrganizeImports", function()
+            local params = {
+                command = "pyright.organizeimports",
+                arguments = { vim.uri_from_bufnr(bufnr) },
+            }
+
+            ---@diagnostic disable-next-line: param-type-mismatch
+            client.request("workspace/executeCommand", params, nil, bufnr)
+        end, {
+            desc = "Organize Imports",
+        })
+        vim.api.nvim_buf_create_user_command(bufnr, "LspPyrightSetPythonPath", set_python_path, {
+            desc = "Reconfigure pyright with the provided python path",
+            nargs = 1,
+            complete = "file",
+        })
+    end,
 }
--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -3,7 +3,7 @@ return {
     init = function()
         vim.g.tex_flavor = "latex"
         vim.g.vimtex_view_general_viewer = "zathura"
-        vim.g.vimtex_compiler_latexmk = { options = { "-auxdir=build" } }
-        vim.g.vimtex_compiler_latexmk_engines = { _ = "-lualatex" }
+        -- vim.g.vimtex_compiler_latexmk = { options = { "-auxdir=build" } }
+        -- vim.g.vimtex_compiler_latexmk_engines = { _ = "-lualatex" }
     end,
 }
